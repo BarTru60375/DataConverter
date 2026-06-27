610b6875a103a6cd4996a13b308c485543e7b80e
BarTru60375 <dsw60375@dsw.edu.pl>
2026-06-27 11:27:00 +0200

    Task3: zapis danych do pliku .json

--- converter.py
+++ converter.py
@@ -43,6 +43,13 @@ def read_json(path):
 
 READERS["json"] = read_json
 
+# Task3: zapis danych z obiektu do pliku w formacie .json
+def write_json(data, path):
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+WRITERS["json"] = write_json
 
 
 
